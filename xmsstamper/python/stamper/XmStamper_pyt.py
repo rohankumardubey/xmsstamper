e5985a3a6877f76a84e5fcf844ec32eb350f6470
Gage Larsen <glarsen@aquaveo.com>
2018-10-11 15:39:12 -0600

    finish tutorial tests

--- xmsstamper/python/stamper/XmStamper_pyt.py
+++ xmsstamper/python/stamper/XmStamper_pyt.py
@@ -326,6 +326,9 @@ class TestXmStamper(unittest.TestCase):
         raster_format_enum = xmsstamper_py.stamper.XmStampRaster.raster_format_enum
         io.raster.write_grid_file(output_file, raster_format_enum.RS_ARCINFO_ASCII)
 
+        # Verify grid file
+        self.assertFilesEqual(base_file, output_file)
+
         base_pts = ((0, 0, 15), (20, 20, 15), (-3.5, 3.5, 15), (-14.1, 14.1, 0),
                     (15.2, 22.2, 15), (4.6, 32.9, 0), (3.5, -3.5, 15),
                     (14.1, -14.1, 0), (24.8, 17.8, 15), (35.4, 7.1, 0),
@@ -340,3 +343,154 @@ class TestXmStamper(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(base_pts, io.get_out_tin().pts, 
                                              decimal=1)
+
+    def test_stamp_guide_bank(self):
+        """Test stamp guidebank"""
+        io = xmsstamper_py.stamper.XmStamperIo()
+
+        # Type and centerline
+        io.stamping_type = 1
+        io.centerline = ((0, 0, 15), (50, 50, 15))
+
+        # Cross sections
+        cs1 = xmsstamper_py.stamper.XmStampCrossSection()
+        cs1.left = ((0, 15), (5, 15), (6, 14))
+        cs1.left_max = 10
+        cs1.index_left_shoulder = 1
+        cs1.right = ((0, 15), (5, 15), (6, 14))
+        cs1.right_max = 10
+        cs1.index_right_shoulder = 1
+        cs2 = xmsstamper_py.stamper.XmStampCrossSection()
+        cs2.left = ((0, 15), (5, 15), (6, 14))
+        cs2.left_max = 10
+        cs2.index_left_shoulder = 1
+        cs2.right = ((0, 15), (5, 15), (6, 14))
+        cs2.right_max = 10
+        cs2.index_right_shoulder = 1
+        io.cs = (cs1, cs2)
+
+        # change to guidebank end cap
+        io.first_end_cap.type = 0
+        io.first_end_cap.guidebank.n_pts = 10
+        io.first_end_cap.guidebank.radius1 = 30
+        io.first_end_cap.guidebank.radius2 = 15
+        io.first_end_cap.guidebank.side = 0 # Left side
+        io.first_end_cap.guidebank.width = 6
+
+        io.last_end_cap.type = 0
+        io.last_end_cap.guidebank.n_pts = 10
+        io.last_end_cap.guidebank.radius1 = 30
+        io.last_end_cap.guidebank.radius2 = 15
+        io.last_end_cap.guidebank.side = 0 # Left side
+        io.last_end_cap.guidebank.width = 6
+
+        io.last_end_cap.angle = 10
+
+        # Raster to stamp
+        no_data = -9999999
+        num_pixels_x = 90
+        num_pixels_y = 81
+        pixel_size = 1
+        min_pt = (-21.0, -12.0)
+        raster_vals = [no_data] * (num_pixels_x * num_pixels_y)
+        raster = xmsstamper_py.stamper.XmStampRaster()
+        raster.no_data = no_data
+        raster.num_pixels_x = num_pixels_x
+        raster.num_pixels_y = num_pixels_y
+        raster.pixel_size_x = pixel_size
+        raster.pixel_size_y = pixel_size
+        raster.min = min_pt
+        raster.vals = raster_vals
+        io.raster = raster
+
+        st = xmsstamper_py.stamper.XmStamper()
+        st.do_stamp(io)
+
+        base_file = os.path.join(
+            self.base_file_path, "stamping", "rasterTestFiles", 
+            "testGuidebank_out.asc"
+        )
+        output_file = os.path.join(
+            self.output_file_path, "test_guidebank_out.asc"
+        )
+
+        raster_format_enum = xmsstamper_py.stamper.XmStampRaster.raster_format_enum
+        io.raster.write_grid_file(output_file, raster_format_enum.RS_ARCINFO_ASCII)
+
+        # Verify grid file
+        self.assertFilesEqual(base_file, output_file)
+
+    def test_stamp_intersect_bathymetry(self):
+        """Test stamp guidebank"""
+        io = xmsstamper_py.stamper.XmStamperIo()
+
+        # Type and centerline
+        io.stamping_type = 1
+        io.centerline = ((0, 0, 15), (10, 10, 15))
+
+        # Cross sections
+        cs1 = xmsstamper_py.stamper.XmStampCrossSection()
+        cs1.left = ((0, 15), (5, 15), (6, 14))
+        cs1.left_max = 20
+        cs1.index_left_shoulder = 1
+        cs1.right = ((0, 15), (5, 15), (6, 14))
+        cs1.right_max = 20
+        cs1.index_right_shoulder = 1
+        cs2 = xmsstamper_py.stamper.XmStampCrossSection()
+        cs2.left = ((0, 15), (5, 15), (6, 14))
+        cs2.left_max = 20
+        cs2.index_left_shoulder = 1
+        cs2.right = ((0, 15), (5, 15), (6, 14))
+        cs2.right_max = 20
+        cs2.index_right_shoulder = 1
+        io.cs = (cs1, cs2)
+
+        # Create a TIN to represent bathymetry
+        tin = xmsinterp_py.triangulate.TrTin()
+        pts = ((-1, 25, 6), (-15, 11, 6), (5, -11, 10), (20, 4, 10))
+        tris = (0, 1, 2, 1, 3, 2)
+        tin.set_points(pts)
+        tin.set_triangles(tris)
+        io.bathymetry = tin
+
+        # Raster to stamp
+        no_data = -9999999
+        num_pixels_x = 29
+        num_pixels_y = 34
+        pixel_size = 1
+        min_pt = (-10.0, -8.0)
+        raster_vals = [no_data] * (num_pixels_x * num_pixels_y)
+        raster = xmsstamper_py.stamper.XmStampRaster()
+        raster.no_data = no_data
+        raster.num_pixels_x = num_pixels_x
+        raster.num_pixels_y = num_pixels_y
+        raster.pixel_size_x = pixel_size
+        raster.pixel_size_y = pixel_size
+        raster.min = min_pt
+        raster.vals = raster_vals
+        io.raster = raster
+
+        st = xmsstamper_py.stamper.XmStamper()
+        st.do_stamp(io)
+
+        base_file = os.path.join(
+            self.base_file_path, "stamping", "rasterTestFiles", 
+            "testIntersectBathymetry_out.asc"
+        )
+        output_file = os.path.join(
+            self.output_file_path, "test_intersect_bathymetry_base.asc"
+        )
+
+        raster_format_enum = xmsstamper_py.stamper.XmStampRaster.raster_format_enum
+        io.raster.write_grid_file(output_file, raster_format_enum.RS_ARCINFO_ASCII)
+
+        # Verify grid file
+        self.assertFilesEqual(base_file, output_file)
+
+        base_pts = ((0, 0, 15), (10, 10, 15), (-3.54, 3.54, 15),
+                    (-9.42, 9.42, 6.68), (6.46, 13.54, 15), (-4.14, 24.14, 0),
+                    (3.54, -3.54, 15), (7.18, -7.18, 9.84), (13.54, 6.46, 15),
+                    (17.18, 2.82, 9.84))
+
+        np.testing.assert_array_almost_equal(base_pts, io.get_out_tin().pts,
+                                             decimal=2)
