c7d9e8b3c31fdfbda0d2b654c3c5430e61dfa03e
aclark <aclark@aquaveo.com>
2020-10-13 15:43:51 -0600

    Update requirements.

--- conanfile.py
+++ conanfile.py
@@ -59,9 +59,9 @@ class XmsstamperConan(ConanFile):
         if self.options.pybind:
             self.requires("pybind11/2.5.0@aquaveo/testing")
 
-        self.requires("xmscore/4.0.0-rc14@aquaveo/testing")
-        self.requires("xmsgrid/5.0.0-rc3@aquaveo/testing")
-        self.requires("xmsinterp/4.0.0-rc1@aquaveo/testing")
+        self.requires("xmscore/4.0.0@aquaveo/stable")
+        self.requires("xmsgrid/5.0.2@aquaveo/stable")
+        self.requires("xmsinterp/4.0.0@aquaveo/stable")
 
     def build(self):
         cmake = CMake(self)
@@ -104,10 +104,11 @@ class XmsstamperConan(ConanFile):
                 self.run('python -m unittest discover -v -p *_pyt.py -s {}/_package/tests'.format(
                     os.path.join(self.build_folder)), cwd=os.path.join(self.package_folder, "_package"))
                 # Create and upload wheel to PyPi if release and windows
-                is_release = self.env.get("RELEASE_PYTHON", 'False')
-                if is_release == 'True' and ((self.settings.os == "Macos" or self.settings.os == "Linux")
-                                             or (self.settings.os == "Windows" and
-                                             str(self.settings.compiler.runtime) == "MD")):
+                is_release = self.env.get("RELEASE_PYTHON", 'False') == 'True'
+                is_mac_os = self.settings.os == 'Macos'
+                is_gcc_6 = self.settings.os == "Linux" and float(self.settings.compiler.version.value) == 6.0
+                is_windows_md = (self.settings.os == "Windows" and str(self.settings.compiler.runtime) == "MD")
+                if is_release and (is_mac_os or is_gcc_6 or is_windows_md):
                     devpi_url = self.env.get("AQUAPI_URL", 'NO_URL')
                     devpi_username = self.env.get("AQUAPI_USERNAME", 'NO_USERNAME')
                     devpi_password = self.env.get("AQUAPI_PASSWORD", 'NO_PASSWORD')
